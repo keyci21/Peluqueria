ba8c2d01e6a7432f895ee7549bb2839dd1fc2230
keyci <chinztenorio@gmail.com>
2021-07-17 19:38:27 +0200

    Se agrega el proyecto

---
+++ contacto/correos.php
@@ -0,0 +1,15 @@
+<?php
+    $destinatario = 'chinztenorio@gmail.com';
+    $nombre = $_POST['nombre'];
+    $asunto = $_POST['asunto'];
+    $mensaje = $_POST['mensaje'];
+    $email = $_POST['email'];
+
+    $header = "Enviado desde la pagina de Peluqueria";
+    $mensajeCompleto = $mensaje . "\nAtentamente: " . $nombre;
+
+    mail($destinatario, $asunto, $mensajeCompleto, $header);
+    echo "<script>alert('correo enviado exitosamente')</script>";
+    echo "<script> setTimeout(\"location.href='index.html'\",1000)</script>";
+
+?>
